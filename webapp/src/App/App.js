5b6410832f007b0ac4d1ae454b4450331002d807
Somanwita Dey <dey.so@northeastern.edu>
2020-11-23 22:05:16 -0500

    Initial Commit - Frontend Boilerplate Code

---
+++ webapp/src/App/App.js
@@ -0,0 +1,30 @@
+import './App.scss';
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+
+}
+
+export default App;
