04ad7ba0f727212826d459dfde60f6dee00bd768
Somanwita Dey <dey.so@northeastern.edu>
2020-12-01 17:08:39 -0500

    Login Register Form - Authentication

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,22 +1,25 @@
 import express from 'express';
-import LoginController from '../controllers/login.controller';
+import AuthController from '../controllers/auth.controller';
 
 const router = express.Router();
 
-// search- GET /login
-// create- POST/ login
+router.get("/login", function(req, res) {
+  res.render("login");
+});
+
+router.get("/register", function(req, res) {
+  res.render("register");
+});
+
+// get- GET /login
+// post- POST/ login
 router.route('/login')
-.get(TodoController.index)
-.post(TodoController.create);
+.post(AuthController.create);
 
-// retrieve - GET /todo/${id}
-// update - GET /todo/${id}
-// delete - DELETE /todos/${id}
-
-router.route('/todos/:id')
-.get(TodoController.get)
-.put(TodoController.update)
-.delete(TodoController.remove)
+// get- GET /register
+// post- POST/ register
+router.route('/register')
+.post(AuthController.update)
 
 //module.exports = router;
 
