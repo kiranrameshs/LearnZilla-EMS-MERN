320355e83f06eb23cb0ead18b19fc21d78cdc9ec
Somanwita Dey <dey.so@northeastern.edu>
2020-12-11 09:52:48 -0500

    Added Sidebar

--- webapp/src/components/SideBar/SideBar.js
+++ webapp/src/components/SideBar/SideBar.js
@@ -24,7 +24,81 @@ class Sidebar extends Component {
 }
 
 
+/** logout is a different function in link list*/
+logout(e){
+  this.props.logoutUser()
+}
 
+/** Generating asll links depending on user role*/
+generateLinks(menuItems){
+    return menuItems.map((exp,i) => {
+
+      let isResponsive = this.props.isResponsive;
+      if (isResponsive) {
+        return (<NavItem key={i} componentClass='span'>
+                <Link replace to={{pathname: exp.url}}>  {exp.name} </Link>
+            </NavItem>)
+
+      }else{
+        return (<li key={i} className="nav-item nav-item-education">
+                <Link replace to={{pathname: exp.url}}>  {exp.name} </Link>
+            </li>)
+      }
+    })
+  }
+
+  render() {
+    /*role --> student = 0 and admin = 1*/
+    let role = this.props.auth.role
+    let isResponsive = this.props.isResponsive;
+
+
+    let menuItems = this.state.menu.filter(el => {
+      if (role === 1){
+        if (el.role === 2) {
+          return false
+        }
+        return true
+      }
+
+      if (role === 0) {
+        if (el.role === 0 || el.role === 2) {
+          return true;
+        }
+      }
+      return false
+    })
+    let finalLinks = this.generateLinks(menuItems)
+
+    if (isResponsive) {
+
+
+      return(<Nav className="hidden-md hidden-lg">
+            {finalLinks}
+            <NavItem key="logout" componentClass='span'>
+                    <Link replace to="/login" onClick={this.logout}>  Logout </Link>
+                </NavItem>
+            </Nav>
+          )
+
+    }else{
+
+      return (
+          <nav className="col-md-2 hidden-xs hidden-sm sidebar">
+            <div className="sidebar-sticky">
+              <ul className="nav flex-column">
+                      {finalLinks}
+                      <li key="logout" className="nav-item nav-item-education">
+                          <a href="/login" onClick={this.logout}>  Logout </a>
+                      </li>
+              </ul>
+            </div>
+          </nav>
+      )
+
+    }
+  }
+}
 
 
 
