74d19dc8f81548c03a31ff408a132adba99e0df7
ChandanaSriGandham <gandham.c@husky.neu.edu>
2020-12-11 02:14:19 -0500

    adding Unit tests for Grades and NavBar Components

--- webapp/src/tests/App.test.js
+++ webapp/src/tests/App.test.js
@@ -1,8 +1,54 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import NavBar from '../components/NavBar';
+import AllGradesContainer from '../components/Grades/AllGradesContainer';
+import CourseHWScores from '../components/Grades/CourseHWScores';
+
+
+// Get mock data for API simulation
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+// Renders NavBar Component
+describe('Testing <NavBar /> Component', () => {
+  const wrapper = shallow(
+    <Provider store={store}>
+      <NavBar />
+    </Provider>)
+  it('renders', () => {
+    expect(wrapper.length).toBe(1);
+  });
+})
+
+// Renders AllGradesContainer Component
+describe('Testing <AllGradesContainer /> Component', () => {
+  const wrapper = shallow(
+    <Provider store={store}>
+      <AllGradesContainer />
+    </Provider>)
+  it('renders', () => {
+    expect(wrapper.length).toBe(1);
+  });
+})
+
+// Renders CourseHWScores Component
+describe('Testing <CourseHWScores /> Component', () => {
+  const wrapper = shallow(
+    <Provider store={store}>
+      <CourseHWScores />
+    </Provider>)
+  it('renders', () => {
+    expect(wrapper.length).toBe(1);
+  });
+})
+
+
+
+
