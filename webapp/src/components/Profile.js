86088a5a698097aa552adc1d774d2d2d8250b48a
ChandanaSriGandham <gandham.c@husky.neu.edu>
2020-12-11 21:04:50 -0500

    adding profile component

---
+++ webapp/src/components/Profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import NavBar from '../NavBar';
+
+
+class Profile extends Component {
+
+    render() {
+        return(
+            <>
+             <NavBar />
+             
+            </>
+            
+
+
+        );
+      
+    }
+}
+
+export default Profile
