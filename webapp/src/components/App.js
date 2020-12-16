e089f46782743be4b7c25a30edfd586f909118ca
Somanwita Dey <dey.so@northeastern.edu>
2020-12-16 03:01:58 -0500

    Update GradeCourse component - teacher can grade course final score for students

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -13,6 +13,7 @@ import GradeAnalytics from './Grades/GradeAnalytics';
 import Profile from './Profile/ProfileContainer';
 import SuccessPage from './SuccessPage/SuccessPage';
 import GradeStudents from './Grades/GradeStudents';
+import GradeCourse from './Grades/GradeCourse';
 
 class App extends Component {
 
@@ -33,6 +34,7 @@ class App extends Component {
             <Route path="/students/edit" component={GradeStudents} />
             <Route path="/assignments/create" component={CreateAssignments} />
             <Route path="/home" component={NavBar} />
+            <Route path="/students/grade" component={GradeCourse} />
             {/* <Route path="/grades" component={AllGradesContainer} /> */}
             {/* <Route path="/HW-scores" component={CourseAssignScores} /> */}
             {/* <Route path="/" component={Home} /> */}
